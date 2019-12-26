60126fc5abb1665edb6f794766ad1dd71addece5
sebastjan99 <38669826+sebastjan99@users.noreply.github.com>
2019-12-26 11:05:09 +0100

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,19 +1,10 @@
-//Constants which define keyboard buttons for event.key.
+const Input_ResetCamera = "r";
 const Input_RotateButton = 0;
 const Input_PanButton = 1;
-const Input_ResetCamera = "r";
-const Input_MoveLeft = "ArrowLeft";
-const Input_MoveRight = "ArrowRight";
-const Input_MoveForward = "ArrowUp";
-const Input_Movebackward = "ArrowDown";
 
 export
 {
     Input_ResetCamera,
     Input_RotateButton,
-    Input_PanButton,
-    Input_MoveLeft,
-    Input_MoveRight,
-    Input_MoveForward,
-    Input_Movebackward
+    Input_PanButton
 };
